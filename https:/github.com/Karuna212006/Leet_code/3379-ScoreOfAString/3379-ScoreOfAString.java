f49e698986f4853180a432b1f0982eb64dcf092c
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 13:58:34 +0530

    Create 3379-ScoreOfAString

---
+++ https:/github.com/Karuna212006/Leet_code/3379-ScoreOfAString/3379-ScoreOfAString.java
@@ -0,0 +1,11 @@
+// Last updated: 7/14/2026, 1:58:30 PM
+class Solution {
+    public int scoreOfString(String s) {
+         int sum=0;
+        for(int i=0;i<s.length()-1;i++){
+            sum=sum+Math.abs(s.charAt(i)- s.charAt(i+1));
+            }
+        
+        return sum;
+    }
+}
