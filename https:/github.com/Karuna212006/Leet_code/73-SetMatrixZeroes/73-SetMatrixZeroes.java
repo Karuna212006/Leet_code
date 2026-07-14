4286a10825f2070d526b3fa104c20b8f62ff7f8e
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:00:48 +0530

    Create 73-SetMatrixZeroes

---
+++ https:/github.com/Karuna212006/Leet_code/73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -0,0 +1,29 @@
+// Last updated: 7/14/2026, 2:00:44 PM
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        Set<Integer> zeroRows=new HashSet<>();
+        Set<Integer> zerocols=new HashSet<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    zeroRows.add(i);
+                    zerocols.add(j);
+                }
+            }
+        }
+
+        for(int r:zeroRows){
+            for(int j=0;j<n;j++){
+                    matrix[r][j]=0;
+            }
+        }
+
+        for(int c:zerocols){
+            for(int i=0;i<m;i++){
+                matrix[i][c]=0;
+            }
+        }
+    }
+}
