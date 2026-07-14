3964f56146ee9a62ce0f99aa13bf3b1b75c869ae
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:00:03 +0530

    Create 198-HouseRobber

---
+++ https:/github.com/Karuna212006/Leet_code/198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 1:59:59 PM
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1)return nums[0];
+        int []dp=new int[n];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[1],nums[0]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
+    }
+}
