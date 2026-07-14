5904380cf7eba660cc9fb57ab82366965be3c545
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 13:59:34 +0530

    Create 338-CountingBits

---
+++ https:/github.com/Karuna212006/Leet_code/338-CountingBits/338-CountingBits.java
@@ -0,0 +1,10 @@
+// Last updated: 7/14/2026, 1:59:30 PM
+class Solution {
+    public int[] countBits(int n) {
+        int []dp=new int[n+1];
+        for(int i=1;i<=n;i++){
+            dp[i]=dp[i/2]+(i&1);
+        }
+        return dp;
+    }
+}
