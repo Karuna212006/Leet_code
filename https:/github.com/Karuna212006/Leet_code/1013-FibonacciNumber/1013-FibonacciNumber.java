f01c7dc2d55b22111dd88663dc507362d0012436
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 13:58:50 +0530

    Create 1013-FibonacciNumber

---
+++ https:/github.com/Karuna212006/Leet_code/1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 1:58:46 PM
+class Solution {
+    public int fib(int n) {
+        if(n==0)return 0;
+        if(n==1)return 1;
+        int []dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<dp.length;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+            return dp[n];
+    }
+}
