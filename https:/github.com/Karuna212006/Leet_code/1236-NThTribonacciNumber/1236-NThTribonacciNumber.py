24ec8e7f1a34d9ee946e7577d2e8b1be862f5640
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 13:58:46 +0530

    Create 1236-NThTribonacciNumber

---
+++ https:/github.com/Karuna212006/Leet_code/1236-NThTribonacciNumber/1236-NThTribonacciNumber.py
@@ -0,0 +1,17 @@
+# Last updated: 7/14/2026, 1:58:42 PM
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
+
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+        return dp[n]
