a75e7677efcfb7d3c1f485c53e42a365d5478372
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:01:31 +0530

    Create 12-IntegerToRoman

---
+++ https:/github.com/Karuna212006/Leet_code/12-IntegerToRoman/12-IntegerToRoman.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 2:01:27 PM
+class Solution {
+    public String intToRoman(int n) {
+        int [] num={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String [] rom={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        String ans="";
+        for(int i=0;i<num.length;i++){
+            while(n>=num[i]){
+                ans+=rom[i];
+                n-=num[i];
+            }
+        }
+        return ans;
+    }
+}
