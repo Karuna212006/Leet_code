bd09771dd763c58c9864915a10257915c46ae338
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 13:59:23 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ https:/github.com/Karuna212006/Leet_code/387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 1:59:19 PM
+class Solution {
+    public int firstUniqChar(String s) {
+        int frq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            frq[s.charAt(i)-'a']++;
+        }
+       for (int i = 0; i < s.length(); i++) {
+            if (frq[s.charAt(i)-'a'] == 1) {
+        return i;
+    }
+}
+return -1;
+    }
+}
