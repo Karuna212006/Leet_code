6f6f74be01b7f17c56e716534800d6bf73c6219c
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 13:59:16 +0530

    Create 412-FizzBuzz

---
+++ https:/github.com/Karuna212006/Leet_code/412-FizzBuzz/412-FizzBuzz.py
@@ -0,0 +1,20 @@
+# Last updated: 7/14/2026, 1:59:12 PM
+class Solution:
+    def fizzBuzz(self, n: int):
+        ans = []
+
+        for i in range(1, n + 1):
+
+            if i % 3 == 0 and i % 5 == 0:
+                ans.append("FizzBuzz")
+
+            elif i % 3 == 0:
+                ans.append("Fizz")
+
+            elif i % 5 == 0:
+                ans.append("Buzz")
+
+            else:
+                ans.append(str(i))
+
+        return ans
