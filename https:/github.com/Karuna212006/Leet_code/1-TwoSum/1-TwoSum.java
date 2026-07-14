3f3e88bcb9776b7b7f95f2844c987241b7032f87
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:01:48 +0530

    Create 1-TwoSum

---
+++ https:/github.com/Karuna212006/Leet_code/1-TwoSum/1-TwoSum.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 2:01:44 PM
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int need =target-nums[i];
+        if(map.containsKey(need)){
+            return new int []{map.get(need),i};
+        }
+            map.put(nums[i],i);
+    }
+        return new int[0];
+    }
+}
