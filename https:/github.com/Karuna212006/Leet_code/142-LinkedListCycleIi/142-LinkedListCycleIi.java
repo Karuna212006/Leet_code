976fe0687798e866ac1e5b428032edb3bbbdacc0
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:00:26 +0530

    Create 142-LinkedListCycleIi

---
+++ https:/github.com/Karuna212006/Leet_code/142-LinkedListCycleIi/142-LinkedListCycleIi.java
@@ -0,0 +1,28 @@
+// Last updated: 7/14/2026, 2:00:21 PM
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) { 
+                break;
+            }
+        }
+        
+
+        if (fast == null || fast.next == null) return null;
+        
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow;
+    }
+}
