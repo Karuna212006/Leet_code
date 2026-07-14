0cd56440004439afec7400dd5d77a2d4c3cec5c4
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:01:21 +0530

    Create 19-RemoveNthNodeFromEndOfList

---
+++ https:/github.com/Karuna212006/Leet_code/19-RemoveNthNodeFromEndOfList/19-RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,24 @@
+// Last updated: 7/14/2026, 2:01:17 PM
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+
+        ListNode first=dummy;
+        ListNode second=dummy;
+
+        for(int i=0;i<=n;i++){
+            first=first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+
+        return dummy.next;
+    
+    }
+}
