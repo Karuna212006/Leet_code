9ba4ecd0e3c30b928c4035d3cdff6f76a1154823
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:00:54 +0530

    Create 70-ClimbingStairs

---
+++ https:/github.com/Karuna212006/Leet_code/70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 2:00:50 PM
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1) return 1;
+
+        if(n==2)return 2;
+        int []dp=new int[n];
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n-1];
+    }
+}
