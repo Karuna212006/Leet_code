080f7a064ecce10e60cb08f05055f5d35fac888f
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 13:59:51 +0530

    Create 213-HouseRobberIi

---
+++ https:/github.com/Karuna212006/Leet_code/213-HouseRobberIi/213-HouseRobberIi.py
@@ -0,0 +1,20 @@
+# Last updated: 7/14/2026, 1:59:47 PM
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        def robber1(nums):
+            n=len(nums)
+            if(n==1):
+                return nums[0]
+            dp=[0]*n
+            dp[0]=nums[0]
+            dp[1]=max(nums[0],nums[1])
+            for i in range(2,n):
+                dp[i]=max(nums[i]+dp[i-2],dp[i-1])
+            return dp[-1]
+        nums1=nums[1:len(nums)]
+        nums2=nums[0:-1]
+        m1=robber1(nums1)
+        m2=robber1(nums2)
+        return max(m1,m2)
