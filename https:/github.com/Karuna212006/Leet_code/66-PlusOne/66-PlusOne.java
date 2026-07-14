ec37e087659e95b3887d5d117493d7b465ead296
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:00:53 +0530

    Create 66-PlusOne

---
+++ https:/github.com/Karuna212006/Leet_code/66-PlusOne/66-PlusOne.java
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 2:00:49 PM
+class Solution {
+    public int[] plusOne(int[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] < 9) {
+                arr[i]++;  
+                return arr;
+            }
+            arr[i] = 0;
+        }
+
+        int[] result = new int[n + 1];
+        result[0] = 1; 
+        return result;
+    }
+}
