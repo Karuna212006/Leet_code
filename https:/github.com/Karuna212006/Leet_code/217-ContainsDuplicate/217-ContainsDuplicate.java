b9f8facb550126aee843142ee13d69cd3f1454f8
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 13:59:48 +0530

    Create 217-ContainsDuplicate

---
+++ https:/github.com/Karuna212006/Leet_code/217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 1:59:44 PM
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums); 
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true; 
+            }
+        }
+        return false;
+    }
+}
