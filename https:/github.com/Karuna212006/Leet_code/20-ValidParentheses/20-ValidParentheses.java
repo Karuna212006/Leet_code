424c5ffb54c637db70d1db285e420a97dc19f89d
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:01:20 +0530

    Create 20-ValidParentheses

---
+++ https:/github.com/Karuna212006/Leet_code/20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,24 @@
+// Last updated: 7/14/2026, 2:01:16 PM
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+          if(ch=='{'||ch=='['||ch=='('){
+            stack.push(ch);
+          }
+          else{
+            if(stack.isEmpty()) return false;
+            char top=stack.pop();
+             if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+                    return false;
+                }
+          }
+        }
+    
+    return stack.isEmpty();
+
+        }
+
+    }
