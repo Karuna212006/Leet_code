f68059895a765ba993522352fc1e3f745dd99d77
Karunakaran <145748768+Karuna212006@users.noreply.github.com>
2026-07-14 14:01:18 +0530

    Create 21-MergeTwoSortedLists

---
+++ https:/github.com/Karuna212006/Leet_code/21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 2:01:14 PM
+public class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
